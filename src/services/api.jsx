66c8de8fd22820a4d2d87ef31a91ae384d4940d7
fatihbahadir <hunkarhyme@gmail.com>
2024-05-26 00:00:30 +0300

    Process bug

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.BACKEND_BASE_URL || "http://localhost:8080";
+const BASE_URL = "http://localhost:8080";
+// browser cannot compile process. will be fixed later
 
 export default axios.create({
     baseURL: BASE_URL,
