515a76ee390178ab7e7b7efada3d64b7e97e884d
Orkun Kurul <orkunkurul@webinen.com>
2024-05-29 15:20:19 +0300

    last update

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.BACKEND_BASE_URL || "http://localhost:8080";
+const BASE_URL = import.meta.env.BACKEND_BASE_URL || "http://localhost:8081";
 // browser cannot compile process. will be fixed later
 
 export default axios.create({
